c2313b266fe0df29f371e6caacd053c64d8b662e
artisansprince <artisansprince@gmail.com>
2024-11-06 17:40:24 +0700

    🌱 feat: Menambahkan api public untuk mendapatkan data objek.

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,17 @@ const express = require('express');
 const authRoutes = require('./routes/admin/adminAuthRoutes');
 const categoryRoutes = require('./routes/admin/adminCategoryRoutes');
 const objectRoutes = require('./routes/admin/adminObjectRoutes');
+const publicObjectRoutes = require('./routes/public/publicObjectRoutes');
 
 const app = express();
 app.use(express.json());
 
+// Menambahkan routes untuk admin
 app.use('/api/admin', authRoutes);
 app.use('/api/categories', categoryRoutes);
 app.use('/api/objects', objectRoutes);
+
+// Menambahkan routes untuk public
+app.use('/api/public', publicObjectRoutes);
 
 module.exports = app;
