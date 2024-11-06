c75ed82072216d03ccd3011e1fe996abc0c4a60b
artisansprince <artisansprince@gmail.com>
2024-11-06 10:48:37 +0700

    🌱 feat: Menambahkan tabel object dan category di file migrate-down.js

--- backend/migrations/migration-down.js
+++ backend/migrations/migration-down.js
@@ -3,8 +3,12 @@ const db = require('../config/db');
 
 async function migrateDown() {
     try {
+        // Drop tabel dalam urutan yang aman
+        await db.query('DROP TABLE IF EXISTS categories;');
+        await db.query('DROP TABLE IF EXISTS objects;');
         await db.query('DROP TABLE IF EXISTS admins;');
-        console.log('Migrasi down berhasil untuk tabel admins.');
+
+        console.log('Migrasi down berhasil untuk semua tabel.');
     } catch (error) {
         console.error('Error saat migrasi down:', error);
     } finally {
